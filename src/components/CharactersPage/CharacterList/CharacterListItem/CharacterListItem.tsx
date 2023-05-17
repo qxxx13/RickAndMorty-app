b2c4f54a4adaea2ab865a0497a4fd087ca25de11
Nikolay Nazarenko <redecv9@gmail.com>
2023-05-17 21:15:24 +0300

    Setup episode info for characters

--- src/components/CharactersPage/CharacterList/CharacterListItem/CharacterListItem.tsx
+++ src/components/CharactersPage/CharacterList/CharacterListItem/CharacterListItem.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import React, { useMemo } from "react";
+import { Card, CardContent, CardMedia, Stack, Typography } from "@mui/material";
 
 import { CharacterItemModel } from "../../../../models/CharacterItemModel";
 
@@ -8,15 +8,31 @@ type CharacterItemProps = {
 };
 
 export const CharacterListItem: React.FC<CharacterItemProps> = ({ character }) => {
+    const EpisodeList = useMemo(() => {
+        return character.episode.map((episode) => {
+            return (
+                <Typography key={episode.id} variant="body1">
+                    {episode.name}
+                </Typography>
+            );
+        });
+    }, [character.episode]);
+
     return (
-        <Card sx={{ width: "100%", mb: 2, display: "flex" }} elevation={15}>
+        <Card sx={{ width: "100%", mb: 2, display: "flex", maxHeight: 300 }} elevation={15}>
             <CardMedia sx={{ width: 300, height: 300 }} image={character.image} />
-            <CardContent>
-                <Typography variant="h5">{character.name}</Typography>
+            <CardContent sx={{ maxWidth: 400 }}>
+                <Typography variant="h3">{character.name}</Typography>
                 <Typography variant="body2">{character.gender}</Typography>
                 <Typography variant="body2">{character.species}</Typography>
                 <Typography variant="body2">{character.status}</Typography>
             </CardContent>
+            <Stack sx={{ height: 300, overflowY: "auto", ml: "auto", padding: "16px", textAlign: "right" }}>
+                <Typography variant="h5" sx={{ mb: 1 }}>
+                    Episode:
+                </Typography>
+                {EpisodeList}
+            </Stack>
         </Card>
     );
 };
